87650cc4d746cdc64e73d54c6c1c6836957109c2
Buse Savaş <81926506+buseesavas@users.noreply.github.com>
2024-10-07 02:08:23 +0300

    Add files via upload

---
+++ assets/js/oyun.js
@@ -0,0 +1,33 @@
+let bilgisayarHamlesi = ["taş", "kağıt", "makas"].at(
+  Math.floor(Math.random() * 3)
+);
+let oyuncuHamlesi = prompt("taş mı? kağıt mı? makas mı?");
+if (
+  (bilgisayarHamlesi == "taş" && oyuncuHamlesi == "kağıt") ||
+  (bilgisayarHamlesi == "kağıt" && oyuncuHamlesi == "makas") ||
+  (bilgisayarHamlesi == "makas" && oyuncuHamlesi == "taş")
+) {
+  alert(
+    "Bilgisayar seçimi: " +
+      bilgisayarHamlesi +
+      ". " +
+      "Sen kazandın. Tebrikler :)"
+  );
+} else if (
+  (bilgisayarHamlesi == "taş" && oyuncuHamlesi == "makas") ||
+  (bilgisayarHamlesi == "kağıt" && oyuncuHamlesi == "taş") ||
+  (bilgisayarHamlesi == "makas" && oyuncuHamlesi == "kağıt")
+) {
+  alert(
+    "Bilgisayar seçimi: " +
+      bilgisayarHamlesi +
+      ". " +
+      "Bilgisayar kazandı. Tekrar dene :("
+  );
+} else if (
+  (bilgisayarHamlesi == "taş" && oyuncuHamlesi == "taş") ||
+  (bilgisayarHamlesi == "kağıt" && oyuncuHamlesi == "kağıt") ||
+  (bilgisayarHamlesi == "makas" && oyuncuHamlesi == "makas")
+) {
+  alert("Bilgisayar seçimi: taş. Berabere. Tekrar dene..");
+}
